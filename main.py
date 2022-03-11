13a47fa8542b0396e5b2b55fc8e0b8df952c2a6c
danilachernov <black-d.c@yandex.ru>
2022-03-11 12:26:12 +0300

    Find the roots

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
-sides = [3, 2, 4, 7, 5, 12, 11, 13, 15, 16, 14, 14]
+import math
 
-sides = sorted(sides, reverse=True)
-
-smax = 0
-for i in range(len(sides)):
-    for j in range(i + 1, len(sides)):
-        for k in range(j + 1, len(sides)):
-            a = sides[i]
-            b = sides[j]
-            c = sides[k]
-            if a + b > c and a + c > b and b + c > a:
-                p = (a + b + c) / 2
-                s = (p * (p - a) * (p - b) * (p - c)) ** (1 / 2)
-                if s > smax:
-                    smax = s
-print("Максимальная площадь треугольника", smax)
+a = float(input("a="))
+b = float(input("b="))
+c = float(input("c="))
+dis = b*b+4*a*c
+if dis > 0:
+    x1 = (-b + math.sqrt(dis)) / 2 * a
+    x2 = (-b - math.sqrt(dis)) / 2 * a
+    print("x1=", x1)
+    print("x2=", x2)
+elif dis == 0:
+    x = -b/(2*a)
+    print("x=", x)
+else:
+    print("Корней нет")
